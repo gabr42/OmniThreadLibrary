522a5f1dd1feae0d87070011ee8f1bf31ecbe250
primoz@gabrijelcic.org <primoz@gabrijelcic.org@aaf15e17-9050-0410-8b89-91af94d810dc>
2008-07-02 20:53:25 +0000

    example: TOmniWorker worker, .FreeOnTerminate

---
+++ trunk/tests/6_TwoWayHello_with_object_worker/testOmniThreadLibrary1.pas
@@ -0,0 +1,132 @@
+unit testOmniThreadLibrary1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ActnList,
+  OtlCommon,
+  OtlComm,
+  OtlTask,
+  OtlTaskEvents;
+
+const
+  MSG_CHANGE_MESSAGE = 1;
+  MSG_SEND_MESSAGE   = 2;
+
+type
+  TAsyncHello = class(TOmniWorker)
+  strict private
+    aiMessage: string;
+  public
+    constructor Create(const initialMessage: string);
+    procedure OMChangeMessage(var msg: TOmniMessage); message MSG_CHANGE_MESSAGE;
+    procedure OMSendMessage(var msg: TOmniMessage); message MSG_SEND_MESSAGE;
+  end;
+
+  TfrmTestOTL = class(TForm)
+    actChangeMessage: TAction;
+    ActionList      : TActionList;
+    actStartHello   : TAction;
+    actStopHello    : TAction;
+    btnChangeMessage: TButton;
+    btnStartHello   : TButton;
+    btnStopHello    : TButton;
+    lbLog           : TListBox;
+    OmniTaskEventDispatch1: TOmniTaskEventDispatch;
+    procedure actChangeMessageExecute(Sender: TObject);
+    procedure actChangeMessageUpdate(Sender: TObject);
+    procedure actStartHelloExecute(Sender: TObject);
+    procedure actStartHelloUpdate(Sender: TObject);
+    procedure actStopHelloExecute(Sender: TObject);
+    procedure actStopHelloUpdate(Sender: TObject);
+    procedure OmniTaskEventDispatch1TaskMessage(task: IOmniTaskControl);
+    procedure OmniTaskEventDispatch1TaskTerminated(task: IOmniTaskControl);
+  strict private
+    FHelloTask: IOmniTaskControl;
+  private
+  end;
+
+var
+  frmTestOTL: TfrmTestOTL;
+
+implementation
+
+uses
+  DSiWin32;
+
+{$R *.dfm}
+
+{ TfrmTestOTL }
+
+procedure TfrmTestOTL.actChangeMessageExecute(Sender: TObject);
+begin
+  FHelloTask.Comm.Send(MSG_CHANGE_MESSAGE, 'Random ' + IntToStr(Random(1234)));
+end;
+
+procedure TfrmTestOTL.actChangeMessageUpdate(Sender: TObject);
+begin
+  (Sender as TAction).Enabled := assigned(FHelloTask);
+end;
+
+procedure TfrmTestOTL.actStartHelloExecute(Sender: TObject);
+begin
+  FHelloTask :=
+    OmniTaskEventDispatch1.Monitor(CreateTask(TAsyncHello.Create('Hello'), 'Hello')).
+    SetIdle(1000, MSG_SEND_MESSAGE).
+    SetParameter(1000, 'Delay').
+    FreeOnTerminate.
+    Run;
+end;
+
+procedure TfrmTestOTL.actStartHelloUpdate(Sender: TObject);
+begin
+  (Sender as TAction).Enabled := not assigned(FHelloTask);
+end;
+
+procedure TfrmTestOTL.actStopHelloExecute(Sender: TObject);
+begin
+  FHelloTask.Terminate;
+  FHelloTask := nil;
+end;
+
+procedure TfrmTestOTL.actStopHelloUpdate(Sender: TObject);
+begin
+  (Sender as TAction).Enabled := assigned(FHelloTask);
+end;
+
+procedure TfrmTestOTL.OmniTaskEventDispatch1TaskMessage(task: IOmniTaskControl);
+var
+  msg: TOmniMessage;
+begin
+  task.Comm.Receive(msg);
+  lbLog.ItemIndex := lbLog.Items.Add(Format('[%d/%s] %d|%s',
+    [task.UniqueID, task.Name, msg.msgID, msg.msgData]));
+end;
+
+procedure TfrmTestOTL.OmniTaskEventDispatch1TaskTerminated(task: IOmniTaskControl);
+begin
+  lbLog.ItemIndex := lbLog.Items.Add(Format('[%d/%s] Terminated', [task.UniqueID, task.Name]));
+end;
+
+{ TAsyncHello }
+
+constructor TAsyncHello.Create(const initialMessage: string);
+begin
+  aiMessage := initialMessage;
+end;
+
+procedure TAsyncHello.OMChangeMessage(var msg: TOmniMessage);
+begin
+  if msg.MsgID = MSG_CHANGE_MESSAGE then
+    aiMessage := msg.MsgData;
+end;
+
+procedure TAsyncHello.OMSendMessage(var msg: TOmniMessage);
+begin
+  Task.Comm.Send(0, aiMessage);
+end;
+
+initialization
+  Randomize;
+end.
