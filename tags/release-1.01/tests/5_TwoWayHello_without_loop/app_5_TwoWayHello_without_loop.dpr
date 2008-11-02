9eb81d8c946e3c7fa181f1c177de195d100a9ba9
primoz@gabrijelcic.org <primoz@gabrijelcic.org@aaf15e17-9050-0410-8b89-91af94d810dc>
2008-11-02 17:24:32 +0000

    release 1.01

---
+++ tags/release-1.01/tests/5_TwoWayHello_without_loop/app_5_TwoWayHello_without_loop.dpr
@@ -0,0 +1,20 @@
+program app_5_TwoWayHello_without_loop;
+
+uses
+  Forms,
+  test_5_TwoWayHello_without_loop in 'test_5_TwoWayHello_without_loop.pas' {frmTestTwoWayHello},
+  OtlCommon in '..\..\OtlCommon.pas',
+  OtlTask in '..\..\OtlTask.pas',
+  OtlThreadPool in '..\..\OtlThreadPool.pas',
+  OtlComm in '..\..\OtlComm.pas' {;
+
+{$R *.res};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmTestTwoWayHello, frmTestTwoWayHello);
+  Application.Run;
+end.
