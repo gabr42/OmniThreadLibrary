563a9441978636b581f33f1e745e1033b26f88a8
primoz@gabrijelcic.org <primoz@gabrijelcic.org@aaf15e17-9050-0410-8b89-91af94d810dc>
2009-02-02 09:12:38 +0000

    release 1.02

---
+++ tags/release-1.02/tests/10_Containers/app_10_Containers.dpr
@@ -0,0 +1,16 @@
+program app_10_Containers;
+
+uses
+  Forms,
+  test_10_Containers in 'test_10_Containers.pas' {frmTestOtlContainers},
+  OtlComm in '..\..\OtlComm.pas',
+  OtlTask in '..\..\OtlTask.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmTestOtlContainers, frmTestOtlContainers);
+  Application.Run;
+end.
