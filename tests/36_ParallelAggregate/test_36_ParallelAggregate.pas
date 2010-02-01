568e9f9eef155f5ed408c225d6eed7aeb16670d0
Primož Gabrijelčič <gabr42@gmail.com>
2010-02-01 10:13:17 +0000

    ! less bugs

--- tests/36_ParallelAggregate/test_36_ParallelAggregate.pas
+++ tests/36_ParallelAggregate/test_36_ParallelAggregate.pas
@@ -10,12 +10,13 @@ type
   TfrmParallelAggregateDemo = class(TForm)
     Label1: TLabel;
     inpMaxSummand: TSpinEdit;
-    btnSumNumbers: TButton;
-    Button2: TButton;
-    Label2: TLabel;
-    outSum: TEdit;
-    procedure btnSumNumbersClick(Sender: TObject);
+    btnSumParallel: TButton;
+    lbLog: TListBox;
+    btnSumSerial: TButton;
+    procedure btnSumParallelClick(Sender: TObject);
+    procedure btnSumSerialClick(Sender: TObject);
   private
+    procedure Log(const msg: string; const params: array of const);
     { Private declarations }
   public
     { Public declarations }
@@ -27,21 +28,47 @@ var
 implementation
 
 uses
+  DSiWin32,
   OtlCommon,
   OtlParallel;
 
 {$R *.dfm}
 
-procedure TfrmParallelAggregateDemo.btnSumNumbersClick(Sender: TObject);
+procedure TfrmParallelAggregateDemo.btnSumParallelClick(Sender: TObject);
+var
+  start: int64;
+  sum  : int64;
 begin
-  outSum.Text := IntToStr(
+  start := DSiTimeGetTime64;
+  sum :=
     Parallel.ForEach(1, inpMaxSummand.Value)
     .Aggregate(procedure (var aggregate: TOmniValue; const value: TOmniValue) begin
       aggregate.AsInt64 := aggregate.AsInt64 + value.AsInt64;
     end)
     .Execute(function (const value: TOmniValue): TOmniValue begin
       Result := value;
-    end));
+    end);
+  start := DSiTimeGetTime64 - start;
+  Log('Sum(1..%d) = %d; calculation took %d ms', [inpMaxSummand.Value, sum, start]);
+end;
+
+procedure TfrmParallelAggregateDemo.btnSumSerialClick(Sender: TObject);
+var
+  i    : integer;
+  start: int64;
+  sum  : int64;
+begin
+  start := DSiTimeGetTime64;
+  sum := 0;
+  for i := 1 to inpMaxSummand.Value do
+    Inc(sum, i);
+  start := DSiTimeGetTime64 - start;
+  Log('Sum(1..%d) = %d; calculation took %d ms', [inpMaxSummand.Value, sum, start]);
+end;
+
+procedure TfrmParallelAggregateDemo.Log(const msg: string; const params: array of const);
+begin
+  lbLog.ItemIndex := lbLog.Items.Add(Format(msg, params));
 end;
 
 end.
