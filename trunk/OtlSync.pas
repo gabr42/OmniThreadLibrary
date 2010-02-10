a180a4406c5963ce60e425574cb279a1ddd3b18f
primoz@gabrijelcic.org <primoz@gabrijelcic.org@aaf15e17-9050-0410-8b89-91af94d810dc>
2010-02-10 13:36:40 +0000

    + Move64 replaced - again (this time it works :) )

--- trunk/OtlSync.pas
+++ trunk/OtlSync.pas
@@ -149,7 +149,7 @@ function CAS32(const oldValue: pointer; newValue: pointer; var destination): boo
 function CAS64(const oldData: pointer; oldReference: cardinal; newData: pointer;
   newReference: cardinal; var destination): boolean;
 procedure Move64(var Source, Destination); overload;
-procedure Move64(newData: pointer; newReference: cardinal; var Destination); overload; stdcall;
+procedure Move64(newData: pointer; newReference: cardinal; var Destination); overload; 
 procedure Move128(var Source, Destination);
 
 function GetThreadId: cardinal;
@@ -266,12 +266,13 @@ asm
   movq  qword [Destination], xmm0
 end;
 
-procedure Move64(newData: pointer; newReference: cardinal; var Destination); overload; stdcall;
+procedure Move64(newData: pointer; newReference: cardinal; var Destination); overload; 
 //Move 8 bytes atomically into 8-byte Destination!
 asm
-  movq  xmm0, qword [newData]
-  mov   eax, Destination
-  movq  qword [eax], xmm0
+  movd  xmm0, eax
+  movd  xmm1, edx
+  punpckldq xmm0, xmm1
+  movq  qword [Destination], xmm0
 end; { Move64 }
 
 procedure Move128(var Source, Destination);
