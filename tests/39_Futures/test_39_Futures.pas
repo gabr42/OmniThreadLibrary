6d3f80926a474b9d0991786bf0afd264bdc6d911
Primož Gabrijelčič <gabr42@gmail.com>
2010-06-17 13:03:41 +0000

    + futures refactored into OtlFutures unit * IsSignaled renamed to IsSignalled

--- tests/39_Futures/test_39_Futures.pas
+++ tests/39_Futures/test_39_Futures.pas
@@ -6,37 +6,10 @@ uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls,
   OtlTask,
-  OtlTaskControl;
+  OtlTaskControl,
+  OtlFutures;
 
 type
-  TOmniFutureDelegate<T> = reference to function: T;
-  TOmniFutureFunction<T> = function: T;
-  TOmniFutureMethod<T>   = function: T of object;
-
-  IOmniFuture<T> = interface
-    procedure Cancel;
-    function  IsCanceled: boolean;
-    function  IsDone: boolean;
-    function  Value: T;
-  end; { IOmniFuture<T> }
-
-  { TODO : how to pass cancellation token to the action? }
-  { TODO : maybe add Value(timeout_ms) }
-  TOmniFuture<T> = class(TInterfacedObject, IOmniFuture<T>)
-  strict private
-    ofCanceled: boolean;
-    ofResult  : T;
-    ofTask    : IOmniTaskControl;
-  public
-    constructor Create(action: TOmniFutureDelegate<T>); overload;
-    constructor Create(action: TOmniFutureFunction<T>); overload;
-    constructor Create(action: TOmniFutureMethod<T>); overload;
-    procedure Cancel;
-    function  IsCanceled: boolean; inline;
-    function  IsDone: boolean;
-    function  Value: T;
-  end; { TOmniFuture<T> }
-
   TfrmFuturesDemo = class(TForm)
     btnTestFuture1: TButton;
     lbLog: TListBox;
@@ -79,14 +52,17 @@ begin
   Result := true;
 end;
 
-function CountPrimesToFunc: integer;
+function CountPrimesToFunc(const task: IOmniTask): integer;
 var
   i: integer;
 begin
   Result := 0;
   for i := 1 to CPrimesHigh do
-    if IsPrime(i) then
+    if IsPrime(i) then begin
       Inc(Result);
+      if task.CancellationToken.IsSignalled then
+        Exit;
+    end;
 end;
 
 { TfrmFuturesDemo }
@@ -96,7 +72,7 @@ var
   numPrimes: IOmniFuture<integer>;
 begin
   numPrimes := CountPrimesTo(CPrimesHigh);
-  // do something else
+//   do something else
   lbLog.Items.Add(Format('%d primes from 1 to %d', [numPrimes.Value, CPrimesHigh]));
   lbLog.Items.Add(Format('%d primes from 1 to %d', [numPrimes.Value, CPrimesHigh]));
 end;
@@ -105,6 +81,7 @@ procedure TfrmFuturesDemo.btnTestFuture2Click(Sender: TObject);
 var
   numPrimes: IOmniFuture<integer>;
 begin
+  numPrimes := TOmniFuture<integer>.Create(function: integer begin Result := 0 end);
   numPrimes := TOmniFuture<integer>.Create(function: integer
     var
       i: integer;
@@ -115,7 +92,7 @@ begin
           Inc(Result);
     end
   );
-  // do something else
+//   do something else
   lbLog.Items.Add(Format('%d primes from 1 to %d', [numPrimes.Value, CPrimesHigh]));
 end;
 
@@ -128,7 +105,7 @@ begin
       Result := CountPrimesToHigh(CPrimesHigh);
     end
   );
-  // do something else
+//   do something else
   lbLog.Items.Add(Format('%d primes from 1 to %d', [numPrimes.Value, CPrimesHigh]));
 end;
 
@@ -136,7 +113,8 @@ procedure TfrmFuturesDemo.btnTestFuture4Click(Sender: TObject);
 var
   numPrimes: IOmniFuture<integer>;
 begin
-  numPrimes := TOmniFuture<integer>.Create(CountPrimesToFunc);
+  // a method is automatically upgraded to a delegate
+  numPrimes := TOmniFuture<integer>.Create(CountPrimesToMethod);
   while not numPrimes.IsDone do begin
     Sleep(200);
     lbLog.Items.Add('... not yet done');
@@ -149,17 +127,17 @@ procedure TfrmFuturesDemo.btnTestFuture5Click(Sender: TObject);
 var
   numPrimes: IOmniFuture<integer>;
 begin
-  numPrimes := TOmniFuture<integer>.Create(CountPrimesToMethod);
+  // a function is automatically upgraded to a delegate
+  numPrimes := TOmniFuture<integer>.CreateEx(CountPrimesToFunc);
   Sleep(200);
   if not numPrimes.IsDone then begin
     lbLog.Items.Add('... not yet done, canceling');
     lbLog.Update;
     numPrimes.Cancel;
   end;
-  if numPrimes.IsCanceled then begin
-    lbLog.Items.Add('... computation was canceled');
-    lbLog.Update;
-  end;
+  lbLog.Items.Add(Format('... IsCancelled = %s', [BoolToStr(numPrimes.IsCancelled)]));
+  lbLog.Items.Add(Format('... IsDone = %s', [BoolToStr(numPrimes.IsDone)]));
+  lbLog.Update;
   lbLog.Items.Add(Format('%d primes from 1 to %d', [numPrimes.Value, CPrimesHigh]));
 end;
 
@@ -191,62 +169,5 @@ function TfrmFuturesDemo.CountPrimesToMethod: integer;
 begin
   Result := CountPrimesToHigh(CPrimesHigh);
 end;
-
-{ TOmniFuture<T> }
-
-constructor TOmniFuture<T>.Create(action: TOmniFutureDelegate<T>);
-begin
-  ofTask := CreateTask(procedure (const task: IOmniTask)
-    begin
-      ofResult := action();
-    end,
-     'TOmniFuture action').Schedule;
-end; { TOmniFuture<T>.Create }
-
-constructor TOmniFuture<T>.Create(action: TOmniFutureFunction<T>);
-begin
-  ofTask := CreateTask(procedure (const task: IOmniTask)
-    begin
-      ofResult := action();
-    end,
-     'TOmniFuture action').Schedule;
-end; { TOmniFuture<T>.Create }
-
-procedure TOmniFuture<T>.Cancel;
-begin
-  if IsCanceled then begin
-    ofCanceled := true;
-    if assigned(ofTask) then
-      ofTask.CancellationToken.Signal;
-  end;
-end; { TOmniFuture }
-
-constructor TOmniFuture<T>.Create(action: TOmniFutureMethod<T>);
-begin
-  ofTask := CreateTask(procedure (const task: IOmniTask)
-    begin
-      ofResult := action();
-    end,
-     'TOmniFuture action').Schedule;
-end; { TOmniFuture<T>.Create }
-
-function TOmniFuture<T>.IsCanceled: boolean;
-begin
-  Result := ofCanceled;
-end; { TOmniFuture }
-
-function TOmniFuture<T>.IsDone: boolean;
-begin
-  Result := (not assigned(ofTask)) or ofTask.WaitFor(0);
-end; { TOmniFuture }
-
-function TOmniFuture<T>.Value: T;
-begin
-  if assigned(ofTask) then begin
-    ofTask.Terminate;
-    ofTask := nil;
-  end;
-  Result := ofResult;
-end; { TOmniFuture<T>.Value }
 
 end.
