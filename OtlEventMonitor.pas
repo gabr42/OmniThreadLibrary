4163191560a170b94fbe9b38a6ee2f684eb166d1
Primož Gabrijelčič <gabr42@gmail.com>
2010-07-22 19:05:40 +0000

    + Implemented ProcessMessages.

--- OtlEventMonitor.pas
+++ OtlEventMonitor.pas
@@ -37,10 +37,12 @@
 ///   Contributors      : GJ, Lee_Nover
 ///
 ///   Creation date     : 2008-06-12
-///   Last modification : 2010-07-07
-///   Version           : 1.03
+///   Last modification : 2010-07-22
+///   Version           : 1.04
 ///</para><para>
 ///   History:
+///     1.04: 2010-07-22
+///       - Implemented ProcessMessages.
 ///     1.03: 2010-07-07
 ///       - Internal message window is exposed via the MessageWindow property.
 ///     1.02: 2010-07-01
@@ -102,6 +104,7 @@ type
     function  Detach(const pool: IOmniThreadPool): IOmniThreadPool; overload;
     function  Monitor(const task: IOmniTaskControl): IOmniTaskControl; overload;
     function  Monitor(const pool: IOmniThreadPool): IOmniThreadPool; overload;
+    procedure ProcessMessages;
   published
     property MessageWindow: THandle read emMessageWindow;
     property OnPoolThreadCreated: TOmniPoolThreadEvent read emOnPoolThreadCreated
@@ -218,6 +221,16 @@ begin
   emMonitoredPools.Add(pool.UniqueID, pool);
   Result := pool.SetMonitor(emMessageWindow);
 end; { TOmniEventMonitor.Monitor }
+
+procedure TOmniEventMonitor.ProcessMessages;
+var
+  msg: TMsg;
+begin
+  while PeekMessage(Msg, emMessageWindow, 0, 0, PM_REMOVE) and (Msg.Message <> WM_QUIT) do begin
+    TranslateMessage(Msg);
+    DispatchMessage(Msg);
+  end;
+end; { TOmniEventMonitor.ProcessMessages }
 
 procedure TOmniEventMonitor.WndProc(var msg: TMessage);
 var
