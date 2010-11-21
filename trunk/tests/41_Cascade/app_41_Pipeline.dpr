04b5a3dfc59e143ceee30c35400b0a0fe28e3a31
gabr42 <gabr42@aaf15e17-9050-0410-8b89-91af94d810dc>
2010-11-21 18:53:13 +0000

    - removed leftover 'Invoke' - added FastMM to the 41_Cascade test

--- trunk/tests/41_Cascade/app_41_Pipeline.dpr
+++ trunk/tests/41_Cascade/app_41_Pipeline.dpr
@@ -1,8 +1,9 @@
 program app_41_Pipeline;
 
 uses
+  FastMM4,
   Forms,
-  test_41_Pipeline in 'test_41_Pipeline.pas' {Form17};
+  test_41_Pipeline in 'test_41_Pipeline.pas' {frmPipelineDemo};
 
 {$R *.res}
 
