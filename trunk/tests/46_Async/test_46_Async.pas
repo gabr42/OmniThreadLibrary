bd3d38976da57c04780d1fa5f34a5c06f978ebfb
gabr42 <gabr42@aaf15e17-9050-0410-8b89-91af94d810dc>
2011-04-16 15:03:53 +0000

    + simpler Async demo

--- trunk/tests/46_Async/test_46_Async.pas
+++ trunk/tests/46_Async/test_46_Async.pas
@@ -7,16 +7,12 @@ uses
   Dialogs, StdCtrls,
   OtlComm;
 
-const
-  WM_LOG = WM_USER;
-
 type
   TfrmDemoParallelAsync = class(TForm)
     btnAsync: TButton;
     lbLog: TListBox;
     procedure btnAsyncClick(Sender: TObject);
   private
-    procedure WMLog(var msg: TOmniMessage); message WM_LOG;
   public
   end;
 
@@ -38,36 +34,17 @@ begin
   btnAsync.Enabled := false;
 
   Parallel.Async(
-    procedure (const task: IOmniTask)
-    var
-      i: integer;
+    procedure
     begin
-      task.Comm.Send(WM_LOG, 'Starting');
-      for i := 1 to 10 do begin
-        task.Comm.Send(WM_LOG, i);
-        Sleep(200);
-      end;
-      task.Comm.Send(WM_LOG, 'Completed');
+      Sleep(500);
+      MessageBeep($FFFFFFFF);
     end,
 
     procedure
     begin
       btnAsync.Enabled := true;
-    end,
-
-    Parallel.TaskConfig.OnMessage(Self)
+    end
   );
-
-  for i := 1 to 10 do begin
-    lbLog.ItemIndex := lbLog.Items.Add('MAIN: ' + IntToStr(i));
-    Sleep(400);
-    Application.ProcessMessages;
-  end;
-end;
-
-procedure TfrmDemoParallelAsync.WMLog(var msg: TOmniMessage);
-begin
-  lbLog.ItemIndex := lbLog.Items.Add('BGTASK: ' + msg.MsgData);
 end;
 
 end.
