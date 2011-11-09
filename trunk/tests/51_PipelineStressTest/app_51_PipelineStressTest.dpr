ae4807e40739df25d44a63cd397b6cd3ef735a92
gabr42 <gabr42@aaf15e17-9050-0410-8b89-91af94d810dc>
2011-11-09 07:36:59 +0000

    + [Anton Alisov] Pipeline stress test

---
+++ trunk/tests/51_PipelineStressTest/app_51_PipelineStressTest.dpr
@@ -0,0 +1,16 @@
+program app_51_PipelineStressTest;
+
+uses
+  FastMM4,
+  Forms,
+  test_51_PipelineStressTest in 'test_51_PipelineStressTest.pas' {frmPipelineStressTest};
+
+{$R *.res}
+
+begin
+  // ReportMemoryLeaksOnShutdown := True;
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPipelineStressTest, frmPipelineStressTest);
+  Application.Run;
+end.
