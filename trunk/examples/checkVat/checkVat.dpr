4c1c47353838d304e46b59cbe00cb3278e1b265b
gabr42@gmail.com <gabr42@gmail.com@aaf15e17-9050-0410-8b89-91af94d810dc>
2012-08-16 10:03:11 +0000

    + using SOAP from a background task

---
+++ trunk/examples/checkVat/checkVat.dpr
@@ -0,0 +1,14 @@
+program checkVat;
+
+uses
+  Vcl.Forms,
+  checkVat1 in 'checkVat1.pas' {frmCheckVat};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmCheckVat, frmCheckVat);
+  Application.Run;
+end.
