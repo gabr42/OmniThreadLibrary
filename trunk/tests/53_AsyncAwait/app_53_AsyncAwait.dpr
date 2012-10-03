31cdb3491359b1f14311b3999c75920273c57c00
primoz@gabrijelcic.org <primoz@gabrijelcic.org@aaf15e17-9050-0410-8b89-91af94d810dc>
2012-10-03 17:06:44 +0000

    + Added Async/Await abstraction.

---
+++ trunk/tests/53_AsyncAwait/app_53_AsyncAwait.dpr
@@ -0,0 +1,14 @@
+program app_53_AsyncAwait;
+
+uses
+  Vcl.Forms,
+  test_53_AsyncAwait in 'test_53_AsyncAwait.pas' {frmAsyncAwaitTest};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmAsyncAwaitTest, frmAsyncAwaitTest);
+  Application.Run;
+end.
