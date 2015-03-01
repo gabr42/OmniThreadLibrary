c44e969ead171c252d6725eaef479315a0040172
Primož Gabrijelčič <gabr42@gmail.com>
2015-03-01 15:01:33 +0000

    + project, used to generate UML diagrams

---
+++ doc/OmniThreadLibrary_UML.dpr
@@ -0,0 +1,27 @@
+program OmniThreadLibrary_UML;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  OtlCollections in '..\OtlCollections.pas',
+  OtlComm in '..\OtlComm.pas',
+  OtlCommBufferTest in '..\OtlCommBufferTest.pas',
+  OtlCommon in '..\OtlCommon.pas',
+  OtlCommon.Utils in '..\OtlCommon.Utils.pas',
+  OtlContainerObserver in '..\OtlContainerObserver.pas',
+  OtlContainers in '..\OtlContainers.pas',
+  OtlDataManager in '..\OtlDataManager.pas',
+  OtlEventMonitor in '..\OtlEventMonitor.pas',
+  OtlHooks in '..\OtlHooks.pas',
+  OtlLogger in '..\OtlLogger.pas',
+  OtlParallel in '..\OtlParallel.pas',
+  OtlRegister in '..\OtlRegister.pas',
+  OtlSync in '..\OtlSync.pas',
+  OtlTask in '..\OtlTask.pas',
+  OtlTaskControl in '..\OtlTaskControl.pas',
+  OtlThreadPool in '..\OtlThreadPool.pas';
+
+begin
+end.
