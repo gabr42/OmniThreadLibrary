7246f98a71192537c2c1e86ddf09ee75de8f6f25
gabr42 <primoz@gabrijelcic.org>
2015-07-26 23:08:38 +0200

    FastMM was removed from the distribution

--- unittests/buildandrun.bat
+++ unittests/buildandrun.bat
@@ -9,7 +9,7 @@
   mkdir %otl_ut_root%\dcu\Win64 >nul 2>nul
 
 echo Compiling 32-bit
-  dcc32 TestRunner -b -u..;..\src;..\fastmm -i.. -nsSystem;System.Win;Winapi;Vcl;Vcl.Imaging;Vcl.Samples;Data;Xml -e%otl_ut_root%\exe -n0%otl_ut_root%\dcu\win32 -dCONSOLE_TESTRUNNER >%otl_ut_root%\build.log 2>&1
+  dcc32 TestRunner -b -u..;..\src;..\..\fastmm -i.. -nsSystem;System.Win;Winapi;Vcl;Vcl.Imaging;Vcl.Samples;Data;Xml -e%otl_ut_root%\exe -n0%otl_ut_root%\dcu\win32 -dCONSOLE_TESTRUNNER >%otl_ut_root%\build.log 2>&1
   if errorlevel 1 goto error
 
 echo Running 32-bit
@@ -20,7 +20,7 @@ echo Running 32-bit
   if errorlevel 1 goto exit
 
 echo Compiling 64-bit  
-  dcc64 TestRunner -b -u..;..\src;..\fastmm -i.. -nsSystem;System.Win;Winapi;Vcl;Vcl.Imaging;Vcl.Samples;Data;Xml -e%otl_ut_root%\exe -n0%otl_ut_root%\dcu\win64 -dCONSOLE_TESTRUNNER >%otl_ut_root%\build.log 2>&1
+  dcc64 TestRunner -b -u..;..\src;..\..\fastmm -i.. -nsSystem;System.Win;Winapi;Vcl;Vcl.Imaging;Vcl.Samples;Data;Xml -e%otl_ut_root%\exe -n0%otl_ut_root%\dcu\win64 -dCONSOLE_TESTRUNNER >%otl_ut_root%\build.log 2>&1
   if errorlevel 1 goto error
 
   if not %PROCESSOR_ARCHITECTURE%==AMD64 goto on32bit
