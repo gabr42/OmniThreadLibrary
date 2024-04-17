11a30b23d1f04c13a2e823df0a53bb71faa0ae96
SMelnyk64 <smelnyk@hartreepartners.com>
2024-04-17 14:45:52 -0400

    Update OtlParallel.pas

--- OtlParallel.pas
+++ OtlParallel.pas
@@ -2599,7 +2599,10 @@ begin
           while (not Stopped) and localQueue.GetNext(value) do
             loopBody(task, value, taskState);
         finally FreeAndNil(localQueue); end;
-      finally if Assigned(FTaskFinalizer) then FTaskFinalizer(taskState); end;
+      finally 
+        if Assigned(FTaskFinalizer) then 
+          FTaskFinalizer(taskState); 
+      end;
     end
   );
 end; { TOmniParallelLoopBase.InternalExecute }
